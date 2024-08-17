ddb3f9df75ceb0e48c6bc0d7846bd85df5171182
tomascordero <tcorderoiv@gmail.com>
2024-08-16 22:12:21 -0700

    Removed console log

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ client.once(Events.ClientReady, readyClient => {
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
 	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
